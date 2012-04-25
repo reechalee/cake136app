a8dc0af5be1e72cefe2c3401e38174ff2ae36dbc
Richard Lee <richard@newrelic.com>
2012-04-24 22:56:23 -0700

    add app and cake dir

---
+++ app/tests/cases/controllers/posts_controller.test.php
@@ -0,0 +1,47 @@
+<?php
+/* Posts Test cases generated on: 2011-01-17 00:01:57 : 1295253117*/
+App::import('Controller', 'Posts');
+
+class TestPostsController extends PostsController {
+	var $autoRender = false;
+
+	function redirect($url, $status = null, $exit = true) {
+		$this->redirectUrl = $url;
+	}
+}
+
+class PostsControllerTestCase extends CakeTestCase {
+	var $fixtures = array('app.post');
+
+	function startTest() {
+		$this->Posts =& new TestPostsController();
+		$this->Posts->constructClasses();
+	}
+
+	function endTest() {
+		unset($this->Posts);
+		ClassRegistry::flush();
+	}
+
+	function testIndex() {
+
+	}
+
+	function testView() {
+
+	}
+
+	function testAdd() {
+
+	}
+
+	function testEdit() {
+
+	}
+
+	function testDelete() {
+
+	}
+
+}
+?>
